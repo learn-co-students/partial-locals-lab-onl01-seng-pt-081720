35af6c5759a324feabd6f65356e5a84d9361a80f
Cloudymolecule <baires82@gmail.com>
2021-01-05 10:30:40 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,39 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query == ""
+      @students = Student.all
+    else
+      @students = Student.where("name like ?", "%#{query}%")
+    end
+  end
 end
+
+# def self.search(query)
+#   res = []
+#   a = query.downcase
+#   Student.all.each do |s|
+#     if s.name.downcase == a
+#       res << s
+#     end
+#   end
+#   res
+# end
+
+# def self.search(query)
+#   res = []
+#   if query == ""
+#     @student = Student.all
+#   else
+#     a = query.downcase
+#     Student.all.each do |s|
+#         if s.name.downcase == a
+#           res << s
+#         end
+#     end
+#   end
+#   res
+# end
+
