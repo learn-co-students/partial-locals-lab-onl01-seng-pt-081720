68cbe7315c07abae33f61a4138bc29b2a7a45d9c
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-08 23:40:10 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,21 +15,22 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   #if searching iterate over every student to find a match, else display all students
-  def self.search(query)
-    if query.present?
-      self.all.select do |s|
-        s.name.downcase.include?(query.downcase)
-      end
-    else
-      self.all
-    end
-  end
-
   # def self.search(query)
-  #   if query
-  #     where("name LIKE ?", "%#{query}%")
+  #   if query.present?
+  #     self.all.select do |s|
+  #       s.name.downcase.include?(query.downcase)
+  #     end
   #   else
   #     self.all
   #   end
   # end
+
+  #query in ActiveRecord that follows the form: Student.where("name LIKE ?", "%M%")
+  def self.search(query)
+    if query
+      where("name LIKE ?", "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
